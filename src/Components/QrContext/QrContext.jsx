71b2c29a51719a56e9e569c8d44ad7662682e4e7
Avik Roy <somavik99@gmail.com>
2023-06-27 00:42:02 +0530

    main

--- src/Components/QrContext/QrContext.jsx
+++ src/Components/QrContext/QrContext.jsx
@@ -4,18 +4,18 @@ export const QCodeContext = createContext()
 
 export const QrReducer = (state,action)=>{
     switch(action.type){
-        case "":{
-            return
+        case "" :{
+            return 
         }
         default:
-            return state
+            return state, console.log(action.type)
     }
 }
 
 const QrContext = ({children}) => {
     const [QrState,dispatch] = useReducer(QrReducer, [])
   return (
-    <QCodeContext.Provider>
+    <QCodeContext.Provider value={{QrState:QrState, QrDispatch:dispatch}}>
         {children}
     </QCodeContext.Provider>
   )
