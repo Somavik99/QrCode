81f13cb68df5bb8064e93d74173ee4a24a55618b
Avik Roy <somavik99@gmail.com>
2023-06-27 12:16:53 +0530

    main

--- src/Components/QrCode/QrCode.jsx
+++ src/Components/QrCode/QrCode.jsx
@@ -3,9 +3,17 @@ import { QRCodeCanvas } from "qrcode.react";
 import { motion } from "framer-motion";
 import Form from "../Form/Form";
 import { boxVariants } from "../BoxVariants/BoxVariants";
+import { useState } from "react";
+
 const QrCode = () => {
+  const [QrClick, setQrClick] = useState(false);
+
   const HandleSubmit = (e) => {
     e.preventDefault();
+  };
+
+  const HandleQrClick = () => {
+    setQrClick(!QrClick);
   };
 
   return (
@@ -20,7 +28,6 @@ const QrCode = () => {
         <div className="Qr-Container">
           <div className="QrCode">
             <QRCodeCanvas
-            value={InputState}
               bgColor="#2b7dfa"
               fgColor="#ffffff"
               style={{ width: "140px", height: "140px" }}
@@ -49,24 +56,44 @@ const QrCode = () => {
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level
           </p>
-          <motion.button
-            whileTap={{ scale: 0.9 }}
-            className="gen-btn"
-            style={{
-              textAlign: "center",
-              marginLeft: "-0.5px",
-              marginRight: "20px",
-              paddingBottom: "10px",
-              fontSize: "12px",
-              borderRadius: "10px",
-              padding: "8px",
-            }}
-          >
-            Generate QrCode
-          </motion.button>
+          {!QrClick ? (
+            <motion.button
+              onClick={HandleQrClick}
+              whileTap={{ scale: 0.9 }}
+              className="gen-btn"
+              style={{
+                textAlign: "center",
+                marginLeft: "-0.5px",
+                marginRight: "20px",
+                paddingBottom: "10px",
+                fontSize: "12px",
+                borderRadius: "10px",
+                padding: "8px",
+              }}
+            >
+              Generate QrCode
+            </motion.button>
+          ) : (
+            <motion.button
+              whileTap={{ scale: 0.9 }}
+              className="gen-btn"
+              style={{
+                textAlign: "center",
+                marginLeft: "-0.5px",
+                marginRight: "20px",
+                paddingBottom: "10px",
+                fontSize: "12px",
+                borderRadius: "10px",
+                padding: "8px",
+              }}
+            >
+              Download QrCode
+            </motion.button>
+          )}
         </div>
       </motion.form>
       <motion.div
+        hidden={!QrClick}
         variants={boxVariants.FormBoxVariant}
         initial="FormHidden"
         animate="FormVisible"
